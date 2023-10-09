1ff4dc100c9b9336f36e29e01682cfa564c5f407
Semih Baler <semihbaler@gmail.com>
2023-10-09 12:08:41 +0300

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SurveyMVC.Dtos;
+
+namespace SurveyMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(LoginUserDto user)
+        {
+            return View();
+        }
+    }
+}
---
+++ Dtos/LoginUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SurveyMVC.Dtos
+{
+    public class LoginUserDto
+    {
+        public string Mail { get; set; }
+        public string Password { get; set; }
+    }
+}
